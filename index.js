283835ef892e49b73520d8086704a5141d53c5f0
gusmaooleo <lgcgusmao@gmail.com>
2023-11-22 16:08:24 -0300

    api-update--22-11-23

--- index.js
+++ index.js
@@ -1,65 +1,109 @@
-const express = require('express');
-const cors = require('cors')
-const db = require('./database')
+const express = require("express");
+const cors = require("cors");
+const db = require("./database");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/verify', (req, res) => {
-  const { name, pic_id } = req.body
-  const trim_name = name.trim()
+app.post("/verify", (req, res) => {
+  const { name, pic_id } = req.body;
+  const trim_name = name.trim();
 
-  db('users')
-    .select('*')
+  db("users")
+    .select("*")
     .where({
       user_name: trim_name,
-      pic_id: pic_id
+      pic_id: pic_id,
     })
     .then((rows) => {
       if (rows.length === 0) {
-        res.send(false)
+        res.send(false);
       } else {
-        res.send(true)
+        res.send(true);
       }
     })
     .catch((error) => {
-      console.error(error)
+      console.error(error);
     });
 });
 
-app.post('/push', (req, res) => {
-  const { name, pic_id } = req.body
-  const trim_name = name.trim()
+app.post("/push", (req, res) => {
+  const { name, pic_id } = req.body;
+  const trim_name = name.trim();
 
-  db('users')
+  db("users")
     .insert({
       user_name: trim_name,
-      pic_id: pic_id
+      pic_id: pic_id,
     })
     .then(() => {
-      res.send(true)
+      res.send(true);
     })
     .catch(() => {
-      res.send(false)
+      res.send(false);
     });
 });
 
-app.get('/getID', (req, res) => {
+app.get("/getID", (req, res) => {
   const { name, pic_id } = req.query;
   const trim_name = name.trim();
 
-  db('users')
-    .select('user_id')
+  db("users")
+    .select("user_id")
     .where({
       user_name: trim_name,
-      pic_id: pic_id
+      pic_id: pic_id,
     })
     .then((rows) => {
       res.send(rows[0]);
     })
     .catch((error) => {
-      console.error(error)
+      console.error(error);
+    });
+});
+
+app.post("/getMatchID", (req, res) => {
+  const { userId } = req.body;
+
+  db("matches")
+    .insert({
+      user_id: userId,
+    })
+    .then(() => {
+      db("matches")
+        .select("match_id")
+        .where({
+          user_id: userId,
+        })
+        .then((rows) => {
+          res.send(rows[rows.length - 1]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.post("/insertMatchValues", (req, res) => {
+  const { matchId, matchResult, matchScore, matchMoves, matchTime } = req.body;
+
+  db("matches_stats")
+    .insert({
+      match_id: matchId,
+      score: matchScore,
+      moves: matchMoves,
+      final_result: matchResult,
+      match_time: matchTime,
+    })
+    .then(() => {
+      console.log("data send!");
+    })
+    .catch((error) => {
+      console.log(error);
     });
 });
 
